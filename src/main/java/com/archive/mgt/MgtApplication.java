f7147b92e335cd838ed1061f99f7bcfb89242545
tang <justintang412@gmail.com>
2022-04-07 11:03:29 -0400

    init

---
+++ src/main/java/com/archive/mgt/MgtApplication.java
@@ -0,0 +1,13 @@
+package com.archive.mgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/archive/mgt/MgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.archive.mgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
